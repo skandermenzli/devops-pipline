50381b00ed60c81fed7e9b89fc39c44eeefcc66a
skandermenzli <skandermenzli@insat.u-carthage.tn>
2023-11-15 13:15:22 +0100

    test pipeline 3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven,test pipeline");
+        System.out.println("Hello, Maven,test pipeline 2");
     }
 }
